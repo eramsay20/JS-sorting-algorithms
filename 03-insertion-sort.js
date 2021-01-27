dd7290ff15333ecac4b178b159ba24d82639b15d
Eric Ramsay <eramsay10@gmail.com>
2021-01-27 15:39:33 -0500

    refactored insertionSort

--- 03-insertion-sort.js
+++ 03-insertion-sort.js
@@ -1,30 +1,4 @@
 // Implement Insertion Sort
-const array = [2, -1, 4, 3, 7, 3];
-
-function swap(arr, index1, index2) {
-  return [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
-}
-
-function insertionSort(list) {
-
-   for(let i = 1; i< list.length; i++){ // incrementing your new insert value //
-    for(let j = i - 1; j>= 0; j--){ // decrementing subarr while comparing values
-
-      if(list[i] <= list[j] && (list[i] > list[j-1] || list[j-1] === undefined)){
-          let [ newInsert ]  = list.splice(i, 1); // removes insertValue from right side
-          list.splice(j, 0, newInsert) // inserts new value into sorted array
-          break;
-      }
-    }
-  }
-}
-
-insertionSort(array);
-console.log(array);
-
-module.exports = {
-  insertionSort
-};
 
 // Insertion Sort grows a sorted array on the left side of the input array by:
 // * If it is the first element, it is already sorted. return 1;
@@ -33,3 +7,33 @@ module.exports = {
 // * Shift all the elements in the sorted sub-list that is greater than the value to be sorted
 // * Insert the value
 // * Repeat until list is sorted
+
+
+function insertionSort(arr) {
+  for(let i = 1; i < arr.length; i++){ // loop to grab the 'next' value in unsorted array
+    let valueToInsert = arr[i]; // store it as the value to insert
+    let placeToInsert = i; // store its current index location
+
+    // when the prior input is GREATER than the current input that we want to insert...
+    while((placeToInsert > 0) && (arr[placeToInsert - 1] > valueToInsert)){
+      // copy the larger prior input value OVER the current input
+      arr[placeToInsert] = arr[placeToInsert - 1];
+      // decrement the pointer to the pos of the original
+      // value that was copied, i.e. 'the hole or gap to fill'
+      placeToInsert--;
+    }
+    // then fill in the 'hole' with the value to insert
+    arr[placeToInsert] = valueToInsert;
+  }
+}
+
+// Test Case:
+const array = [2, -1, 4, 3, 7, 3];
+
+insertionSort(array);
+console.log(array);
+
+
+module.exports = {
+  insertionSort
+};
